e663dcdea88d2007f27cd0382f1249be549d39d2
Samiylo Kryshu <skryshu@gmail.com>
2020-03-25 09:50:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+
+
+
+
+def line(array)
+  if array.length < 1 
+    puts "The line is currently empty."
+    
+  elsif array.length >= 1
+    intro = "The line is currently: "
+    placeholder = []
+    array.each do |guest|
+      
+      placeholder << "#{array.index(guest) + 1}. #{guest}"
+    end
+    puts intro + placeholder.join(" ")
+  end
+  
+end
+
+# Start Take a Number method
+
+def take_a_number(line,new_guest)
+  line.push(new_guest)
+  puts "Welcome, #{new_guest}. You are number #{line.index[new_guest] + 1}"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+    
